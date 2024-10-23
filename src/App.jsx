07e2e4afd4fafcbb607ae7a7dcde82a3b587e762
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-10-23 12:50:06 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CatalogPage from './pages/CatalogPage';
-import CamperDetailPage from './pages/CamperDetailPage';
+import AppBarr from './components/AppBarr/AppBarr';
 
-const App = () => {
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const CatalogPage = React.lazy(() => import('./pages/CatalogPage'));
+const CamperDetailsPage = React.lazy(() => import('./pages/CamperDetailsPage'));
+
+function App() {
   return (
     <Router>
-      <Header /> {/* Для навігації по сторінках */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />          {/* Домашня сторінка */}
-        <Route path="/catalog" element={<CatalogPage />} /> {/* Каталог кемперів */}
-        <Route path="/catalog/:id" element={<CamperDetailPage />} /> {/* Окремий кемпер */}
-      </Routes>
+      <AppBarr />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<CamperDetailsPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
-};
+}
 
 export default App;
+
