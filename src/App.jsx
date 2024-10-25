cb75c2da9e914e8b10b1cdca36f143412f3739cd
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-10-25 10:27:20 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AppBarr from './components/AppBarr/AppBarr';
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import CamperDetailsPage from "./pages/CamperDetailsPage/CamperDetailsPage";
 
-const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
-const CatalogPage = React.lazy(() => import('./pages/CatalogPage/CatalogPage'));
-const CamperDetailsPage = React.lazy(() => import('./pages/CamperDetailsPage/CamperDetailsPage'));
-
-function App() {
+function App({ toggleTheme }) {
   return (
-    <Router>
-      <AppBarr />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CamperDetailsPage />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SharedLayout toggleTheme={toggleTheme} />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog/:id" element={<CamperDetailsPage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
-
