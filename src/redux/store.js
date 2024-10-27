94a55abe5fb984fc221fead8d95eb1cc07c2f774
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-27 13:37:13 +0100

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { campersReducer } from "./campers/slice";
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { truckReducer } from './campers/slice';
+import { filtersReducer } from './filters/slice';
+import { favouritesReducer } from './favourites/slice';
+
+
+
+const favouritesPersistConfig = {
+  key: "favourites",
+  storage,
+};
+
+const filtersPersistConfig = {
+  key: "filters",
+  storage,
+};
 
 export const store = configureStore({
-  reducer: { campers: campersReducer },
+  reducer: {
+    trucks:truckReducer,
+    filters: persistReducer(filtersPersistConfig, filtersReducer),
+    favourites:persistReducer(favouritesPersistConfig, favouritesReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
